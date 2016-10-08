926bebccf3f422c00e54e5af1d2c1f485d9d7478
Edmon97 <romahome1997@gmail.com>
2016-10-08 16:08:56 +0300

    Initial Commit

---
+++ DynamicSliderApp/Controllers/NumberController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DynamicSliderApp.Controllers
+{
+    public class NumberController : ApiController
+    {
+        // GET: api/Number/max_value
+        public int Get(int value)
+        {
+            int res = new Random().Next(value+1);
+            return res;
+        }
+    }
+}
---
+++ DynamicSliderApp/scripts/slider.js
@@ -0,0 +1,35 @@
+﻿var app = angular.module('sliderApp', ['rzModule', 'ui.bootstrap']);
+
+app.controller('sliderCtrl', function ($scope, $interval, $http, $location) {
+    var timer;
+    var url = $location.protocol() + "://" + $location.host() + ":" + $location.port() + "/";
+
+    $scope.minRangeSlider = {
+        value: 100,
+        options: {
+            floor: 0,
+            ceil: 1000,
+            step: 1
+        }
+    };
+
+    $scope.start = function () {
+        if (angular.isDefined(timer)) return;
+        timer = $interval(getRandomInt, 2000);
+    }
+
+    $scope.stop = function () {
+        if (angular.isDefined(timer)) {
+            $interval.cancel(timer);
+            timer = undefined;
+        }
+    }
+
+    function getRandomInt() {
+        $http.get(url + "api/Number/" + $scope.minRangeSlider.options.ceil)
+            .then(function (response) {
+                $scope.minRangeSlider.value = response.data;
+            });
+    }
+
+});
